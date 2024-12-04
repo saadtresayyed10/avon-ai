aab3243c95118842100906e276073735d302b024
Saad Sayyed <saadsyed950@gmail.com>
2024-12-04 22:54:29 +0530

    useChat hook added

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import { useChat } from "ai/react";
 import { Send } from "lucide-react";
 
 const HomePage = () => {
+  const { messages, input, handleInputChange, handleSubmit, isLoading, stop } =
+    useChat({ api: "api/genai" });
   return (
     <div className="flex flex-col items-center w-full min-h-screen lg:gap-y-10 p-20">
       {RenderForm()}
