7ea135d977ba9ced15f065e884dc82b63949d801
Saad Sayyed <saadsyed950@gmail.com>
2024-12-04 22:34:16 +0530

    Starting with form

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,21 @@
+import { useChat } from "ai/react";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const { messages, input, handleInputChange, handleSubmit, isLoading, stop } =
+    useChat({ api: "api/genai" });
+
+  return (
+    <div className="flex flex-col items-center w-full min-h-screen lg:gap-y-10 p-20">
+      {RenderForm()}
+    </div>
+  );
+
+  function RenderForm() {
+    return <div>rf</div>;
+  }
+  function RenderMessage() {
+    return <div>rm</div>;
+  }
 };
 
 export default HomePage;
