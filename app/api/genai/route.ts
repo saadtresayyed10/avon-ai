5fcc8db2838e5c245e4085862803a510b4c05897
Saad Sayyed <saadsyed950@gmail.com>
2024-12-04 22:20:21 +0530

    AI Api added

---
+++ app/api/genai/route.ts
@@ -0,0 +1,13 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleGenerativeAIStream, StreamingTextResponse } from "ai";
+
+export async function POST(req: Request, res: Response) {
+  const reqBody = await req.json();
+  const prompt = reqBody.data.prompt;
+
+  const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY!);
+  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+  const streamingResponse = await model.generateContentStream(prompt);
+
+  return new StreamingTextResponse(GoogleGenerativeAIStream(streamingResponse));
+}
