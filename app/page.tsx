4e6df288d12c5a00c3da2f5b85cbf0345890577b
Saad Sayyed <saadsyed950@gmail.com>
2024-12-04 22:52:05 +0530

    Form skeleton ready

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
+"use client";
+
 import { useChat } from "ai/react";
+import { Send } from "lucide-react";
 
 const HomePage = () => {
-  const { messages, input, handleInputChange, handleSubmit, isLoading, stop } =
-    useChat({ api: "api/genai" });
-
   return (
     <div className="flex flex-col items-center w-full min-h-screen lg:gap-y-10 p-20">
       {RenderForm()}
@@ -11,7 +11,14 @@ const HomePage = () => {
   );
 
   function RenderForm() {
-    return <div>rf</div>;
+    return (
+      <form>
+        <input type="text" className="border-2 border-black" />
+        <button type="submit">
+          <Send />
+        </button>
+      </form>
+    );
   }
   function RenderMessage() {
     return <div>rm</div>;
