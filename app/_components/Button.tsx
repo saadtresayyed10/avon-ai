d610b8a28a2801968713a09bcfe07cb8b15b9b89
Saad Sayyed <saadsyed950@gmail.com>
2024-12-07 19:50:18 +0530

    Button component added

---
+++ app/_components/Button.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { ButtonProps } from "../types";
+
+export const Button: React.FC<ButtonProps> = ({
+  children,
+  className = "",
+  onClick,
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`px-16 pt-2.5 pb-1.5 text-4xl font-medium text-pink-500 whitespace-nowrap bg-black rounded-3xl w-[250px] max-md:px-5 ${className}`}
+      tabIndex={0}
+    >
+      {children}
+    </button>
+  );
+};
