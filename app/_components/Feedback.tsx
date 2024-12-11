b5d227896ab61c241bd6af77628e9137d1c9456e
Saad Sayyed <saadsyed950@gmail.com>
2024-12-11 19:21:13 +0530

    Trying to fix problems

--- app/_components/Feedback.tsx
+++ app/_components/Feedback.tsx
@@ -1,21 +1,62 @@
-import { currentUser } from "@clerk/nextjs/server";
+"use client";
 
-const Feedback = async () => {
-  const user = await currentUser();
+import { useUser } from "@clerk/nextjs";
+import { useState } from "react";
+
+const Feedback = () => {
+  const user = useUser();
+  const [message, setMessage] = useState("Hey");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const feedbackData = {
+      message,
+    };
+
+    const response = await fetch("/api/feedback", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(feedbackData),
+    });
+
+    if (response.ok) {
+      alert("Feedback added");
+    } else {
+      alert("Feedback not sent");
+    }
+  };
+
   return (
     <div className="flex justify-center items-center w-full">
-      <form className="flex justify-center items-start flex-col lg:gap-y-6 lg:p-8">
-        <input type="text" placeholder={user?.fullName as string} disabled />
+      <form
+        onSubmit={handleSubmit}
+        className="flex justify-center items-start flex-col lg:gap-y-6 lg:p-8"
+      >
+        <input type="text" value={user.user?.id as string} hidden />
         <input
           type="text"
-          placeholder={user?.emailAddresses[0].emailAddress as string}
+          placeholder={user.user?.fullName as string}
+          value={user.user?.fullName as string}
           disabled
+          readOnly
+        />
+        <input
+          type="text"
+          placeholder={user.user?.emailAddresses[0].emailAddress as string}
+          value={user.user?.emailAddresses[0].emailAddress as string}
+          disabled
+          readOnly
         />
         <textarea
           cols={25}
           rows={6}
           className="border"
           placeholder="You message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
         ></textarea>
         <button type="submit">Submit</button>
       </form>
--- app/api/feedback/route.ts
+++ app/api/feedback/route.ts
@@ -1,10 +1,10 @@
-import { currentUser } from "@clerk/nextjs/server";
+import { useUser } from "@clerk/nextjs";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 export async function POST(request: Request) {
-  const user = await currentUser();
+  const user = useUser();
   const res = await request.json();
 
   const { message } = res;
@@ -12,13 +12,21 @@ export async function POST(request: Request) {
 
   const result = await prisma.feedback.create({
     data: {
-      id: user?.id as string,
-      fullname: user?.fullName as string,
-      email: user?.emailAddresses[0].emailAddress as string,
+      id: user.user?.id as string,
+      fullname: user.user?.fullName as string,
+      email: user.user?.emailAddresses[0].emailAddress as string,
       message,
     },
   });
   console.log("Feedback created", result);
 
   return new Response(JSON.stringify(result), { status: 201 });
+}
+
+export async function GET(request: Request) {
+  const checkingFetch = {
+    message: "O Salma!",
+  };
+
+  return Response.json(checkingFetch);
 }
