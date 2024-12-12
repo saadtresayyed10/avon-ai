e83f291b75bdfb98b59e9323783a0c5460376408
Saad Sayyed <saadsyed950@gmail.com>
2024-12-12 19:15:34 +0530

    Starting with SignedOutPage

--- app/_components/SignedOutPage.tsx
+++ app/_components/SignedOutPage.tsx
@@ -1,7 +1,9 @@
 const SignedOutPage = () => {
   return (
     <div className="flex justify-center items-center w-full min-h-screen bg-white text-black">
-      I am Signed Out
+      <h1 className="lg:text-6xl font-bold uppercase tracking-wide text-center">
+        Your smart companion for creativity, knowledge, and problem-solving.
+      </h1>
     </div>
   );
 };
