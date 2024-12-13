83c8bfda256f2aa72cb0016161e3e6a706d55f9e
Saad Sayyed <saadsyed950@gmail.com>
2024-12-13 22:41:12 +0530

    Starting with animations

--- app/_components/FeedbackShowcase.tsx
+++ app/_components/FeedbackShowcase.tsx
@@ -35,46 +35,59 @@ const FeedbackShowcase = ({ className, ...props }: CardProps) => {
   }, []);
 
   return (
-    <div
-      id="thoughts"
-      className="grid lg:grid-cols-3 grid-cols-1 gap-4 p-10 lg:gap-6"
-    >
-      {feedback.map((fdb) => (
-        <Card key={fdb.id} className={cn("lg:w-[380px]", className)} {...props}>
-          <CardHeader>
-            <CardTitle>{fdb.fullname}</CardTitle>
-            <CardDescription>{fdb.email}</CardDescription>
-          </CardHeader>
-          <CardContent className="grid gap-4">
-            <div className=" flex items-center space-x-4 rounded-md border p-4">
-              <BellRing />
-              <div className="flex-1 space-y-1">
-                <p className="text-sm font-medium leading-none">
-                  Push Notifications
-                </p>
-                <p className="text-sm text-muted-foreground">
-                  Send feedback to your device.
-                </p>
+    <div className="flex justify-center items-center flex-col w-full mt-10">
+      <h1 className="lg:text-6xl text-4xl font-bold uppercase tracking-wide text-center">
+        Feedbacks
+      </h1>
+      <div
+        id="thoughts"
+        className="grid lg:grid-cols-3 grid-cols-1 gap-4 p-10 lg:gap-6"
+      >
+        {feedback.map((fdb) => (
+          <Card
+            key={fdb.id}
+            className={cn("lg:w-[380px]", className)}
+            {...props}
+          >
+            <CardHeader>
+              <CardTitle>{fdb.fullname}</CardTitle>
+              <CardDescription>{fdb.email}</CardDescription>
+            </CardHeader>
+            <CardContent className="grid gap-4">
+              <div className=" flex items-center space-x-4 rounded-md border p-4">
+                <BellRing />
+                <div className="flex-1 space-y-1">
+                  <p className="text-sm font-medium leading-none">
+                    Push Notifications
+                  </p>
+                  <p className="text-sm text-muted-foreground">
+                    Send feedback to your device.
+                  </p>
+                </div>
+                <Switch />
               </div>
-              <Switch />
-            </div>
-            <div>
-              <div className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0">
-                <span className="flex h-2 w-2 translate-y-1 rounded-full bg-black" />
-                <div className="space-y-1">
-                  <p className="text-sm font-medium leading-none">Feedback:</p>
-                  <p className="text-sm text-muted-foreground">{fdb.message}</p>
+              <div>
+                <div className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0">
+                  <span className="flex h-2 w-2 translate-y-1 rounded-full bg-black" />
+                  <div className="space-y-1">
+                    <p className="text-sm font-medium leading-none">
+                      Feedback:
+                    </p>
+                    <p className="text-sm text-muted-foreground">
+                      {fdb.message}
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button className="w-full">
-              Give a Like <Heart />
-            </Button>
-          </CardFooter>
-        </Card>
-      ))}
+            </CardContent>
+            <CardFooter>
+              <Button className="w-full">
+                Give a Like <Heart />
+              </Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 };
