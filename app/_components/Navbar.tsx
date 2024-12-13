59acf166ba2a528b25f009a8785d1309695c58fb
Saad Sayyed <saadsyed950@gmail.com>
2024-12-13 22:49:14 +0530

    Navbar anim done

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,21 +1,24 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { User2 } from "lucide-react";
 import Link from "next/link";
 import MobileMenu from "./MobileMenu";
+import { motion } from "framer-motion";
 
 const hyperlinks = [
   {
     name: "Model",
-    link: "/",
+    link: "https://deepmind.google/technologies/gemini/",
   },
   {
     name: "Socials",
-    link: "/",
+    link: "https://www.linkedin.com/in/saad-sayyed-trev/",
   },
   {
     name: "Portfolio",
-    link: "/",
+    link: "https://sayyedsaad.vercel.app",
   },
 ];
 
@@ -24,15 +27,29 @@ const Navbar = () => {
     <div className="lg:flex hidden justify-between items-center px-20 py-6 bg-white text-black">
       <div className="flex justify-center items-center gap-x-16">
         <Link href="/">
-          <h1 className="font-bold uppercase text-4xl">Avon</h1>
+          <motion.h1
+            initial={{ x: -60, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.4, ease: "easeOut", delay: 0.4 }}
+            className="font-bold uppercase text-4xl"
+          >
+            Avon
+          </motion.h1>
         </Link>
-        <ul className="flex items-center gap-x-4">
+        <motion.ul
+          initial={{ x: 30, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.4, ease: "easeOut", delay: 0.4 }}
+          className="flex items-center gap-x-4"
+        >
           {hyperlinks.map((list, idx) => (
             <li
               key={idx}
               className="text-sm uppercase hover:underline hover:mx-4 duration-300 hover:underline-offset-2 font-medium"
             >
-              <Link href={list.link}>{list.name}</Link>
+              <Link target="_blank" href={list.link}>
+                {list.name}
+              </Link>
             </li>
           ))}
           <SignedIn>
@@ -40,16 +57,21 @@ const Navbar = () => {
               <Link href="/feedback">Thoughts</Link>
             </li>
           </SignedIn>
-        </ul>
+        </motion.ul>
       </div>
       <SignedOut>
         <SignInButton mode="modal">
-          <button className="lg:text-sm bg-neutral-900 px-4 py-2 rounded-md shadow text-white font-semibold uppercase flex items-center gap-x-2 hover:bg-white hover:text-black duration-300 border-black border-2">
+          <motion.button
+            initial={{ x: 20, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.4, ease: "easeOut", delay: 0.4 }}
+            className="lg:text-sm bg-neutral-900 px-4 py-2 rounded-md shadow text-white font-semibold uppercase flex items-center gap-x-2 hover:bg-white hover:text-black duration-300 border-black border-2"
+          >
             Login
             <span>
               <User2 className="w-4 h-4" />
             </span>
-          </button>
+          </motion.button>
         </SignInButton>
       </SignedOut>
       <SignedIn>
